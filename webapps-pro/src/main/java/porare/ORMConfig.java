141f64e0b7fc96d7d05c948c7095efb206cbb3b7
shabbir <sbr.aira44@gmail.com>
2018-12-10 09:05:18 +0530

    webapps commited

---
+++ webapps-pro/src/main/java/porare/ORMConfig.java
@@ -0,0 +1,7 @@
+package porare;
+
+import at.chrl.orm.hibernate.configuration.JPAConfig;
+
+public class ORMConfig extends JPAConfig {
+
+}
---
+++ webapps-pro/src/main/java/porare/SampleController.java
@@ -0,0 +1,13 @@
+package porare;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("sample")
+	public String helloSpring(){
+		return "Hello Spring";
+	}
+}
